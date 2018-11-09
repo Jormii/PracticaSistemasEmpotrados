66a2a8bb2652c7e5d2a82c276afde09d11cce5a7
Jorge López Natal <34511701+Jormii@users.noreply.github.com>
2018-11-09 21:21:27 +0100

    Práctica 1
    El archivo de la práctica 1 ya compila

--- Práctica Obligatoria/Practica1.ino
+++ Práctica Obligatoria/Practica1.ino
@@ -12,43 +12,43 @@ unsigned long debounceDelay = 50;    // the debounce time; increase if the outpu
 
 void setup()
 {
-	for (int i = 0; i < n_pins; i++)
-	{
-		pinMode(PINS_LED[i], OUTPUT);
-	}
-	pinMode(PIN_BOTON, INPUT);
+  for (int i = 0; i < N_PINS; i++)
+  {
+    pinMode(PINS_LED[i], OUTPUT);
+  }
+  pinMode(PIN_BOTON, INPUT);
 }
 
 void loop()
 {
-	int lectura = digitalRead(PIN_BOTON);
-	if (lectura != ultimo_estado_boton)
-	{
-		lastDebounceTime = millis();
-	}
-	
-	if ((millis() - lastDebounceTime) > debounceDelay)
-	{
-		if (lectura != estado_boton)
-		{
-			estado_boton = lectura;
-			// ¿Meter el incremento aqui?
-		}
-		
-		if (estado_boton == HIGH) {
-			contador++;
-		
-			if (contador > MAX_VALOR)
-			{
-				contador = MIN_VALOR;
-			}
-		
-			for (int pin_led = N_PINS - 1; pin_led >= 0; pin_led--)
-			{
-				int k = contador >> pin_led;
-				digitalWrite(PINS_LED[pin_led], k ? 1);
-			}
-		}
-	}
-	ultimo_estado_boton = lectura;
+  int lectura = digitalRead(PIN_BOTON);
+  if (lectura != ultimo_estado_boton)
+  {
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay)
+  {
+    if (lectura != estado_boton)
+    {
+      estado_boton = lectura;
+      // ¿Meter el incremento aqui?
+    }
+
+    if (estado_boton == HIGH) {
+      contador++;
+
+      if (contador > MAX_VALOR)
+      {
+        contador = MIN_VALOR;
+      }
+
+      for (int pin_led = N_PINS - 1; pin_led >= 0; pin_led--)
+      {
+        int k = contador >> pin_led;
+        digitalWrite(PINS_LED[pin_led], k & 1);
+      }
+    }
+  }
+  ultimo_estado_boton = lectura;
 }
